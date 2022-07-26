d241da02e62c3bb954b9bd1bb83f879148872325
truongnqph14659 <77135926+truongnqph14659@users.noreply.github.com>
2022-07-26 10:12:58 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Navigo from 'navigo'
+import { home } from './pages/home'
+const router = new Navigo('/', { linksSelector: 'a' })
+document.addEventListener('DOMContentLoaded', async () => {
+  async function print(Page, id) {
+    const app = document.querySelector('#app')
+    if (app) {
+      app.innerHTML = await Page.render(id)
+    }
+    if (Page.afterRender) Page.afterRender()
+  }
+  router.on({
+    '/': () => print(home),
+  })
+  router.resolve()
+})
---
+++ src/pages/home.js
@@ -0,0 +1,5 @@
+export const home = {
+  render() {
+    return /*html*/ `<h1 class="text-amber-900">day la ai</h1>`
+  },
+}
